97ebc76b8b73fb0c7a68f405ddd1a582d022049e
Michael Wigley <md.wigley@hotmail.com>
2025-09-09 16:06:18 -0600

    ✨ Updated BaseViewModel to use ObservableValidator (#95)
    resolves #94

--- PlugHub.Shared/ViewModels/BaseViewModel.cs
+++ PlugHub.Shared/ViewModels/BaseViewModel.cs
@@ -5,7 +5,7 @@ namespace PlugHub.Shared.ViewModels
     /// <summary>
     /// Base class for all view models in PlugHub, providing property change notification.
     /// </summary>
-    public abstract partial class BaseViewModel : ObservableObject
+    public abstract partial class BaseViewModel : ObservableValidator
     {
     }
 }
